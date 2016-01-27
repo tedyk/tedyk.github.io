ebd03da8c0a4f5ac5eccabc16673f88f55f35e7d
Tedy Kanjirathinkal <tedy.dk@gmail.com>
2016-01-26 22:24:54 -0600

    Update blogger_callbacks.js

--- blogger_callbacks.js
+++ blogger_callbacks.js
@@ -1,4 +1,9 @@
+function SortByTitle(x,y) {
+    return ((x.title.$t == y.title.$t) ? 0 : ((x.title.$t> y.title.$t) ? 1 : -1 ));
+}
+
 function listPosts(jsondata) {
+    jsondata.feed.entry.sort(SortByTitle);
     document.write('<ul>');
     for (var i = 0; i < jsondata.feed.entry.length; i++) {
         for (var j = 0; j < jsondata.feed.entry[i].link.length; j++) {
